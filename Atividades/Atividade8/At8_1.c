130d39366cfb6a97a7567f2e094342b251026a88
AugustoLisboa <lisbo.augusto98@gmail.com>
2021-05-21 11:34:56 -0400

    SOCORRO JESUS

--- Atividades/Atividade8/At8_1.c
+++ Atividades/Atividade8/At8_1.c
@@ -1,39 +1,47 @@
 #include <stdio.h>
 int main (void){
   //se o numero entrar no terceiro vetor atribuir -1 ao numero, executar algo somente se o numero for diferente de -1
-    int tam1,tam2,i,j,h=0,leitor1[15],leitor2[15],final[15];
+    int tam1,tam2,tamf,i,j,h=0,leitor1[15],leitor2[15],final[15],inter;
     do{
-        scanf(" %d",&tam1);
+        scanf ("%d",&tam1);
         if(tam1!=51){
-            for(i=0;i<=tam1-1;i++){
-                scanf(" %d",&leitor1[i]);
+            for (i=0;i<=tam1-1;i++){
+                scanf("%d",&leitor1[15]);
             }
-            scanf(" %d",&tam2);
+            scanf("%d",&tam2);
             for(j=0;j<=tam2-1;j++){
-                scanf(" %d",&leitor2[j]);
+                scanf("%d",&leitor2);
             }
-
-            for(i=0;i<=tam1-1;i++){
-                for(j=0;j<=tam2-1;j++){
-                    if(leitor1[i]!=-1 && leitor2[j]!=1){
-                        if(leitor1[i]<leitor2[j]){
-                            final[h]=leitor1[i];
-                            h++;
-                            leitor1[i]=-1;
-                        }
-                        else{
-                            final[h]=leitor2[j];
-                            h++;
-                            leitor2[j]=-1;
-                        }
-                    }
+            if(leitor1[1]>leitor1[2]){
+                for(i=tam1-1;i>=0;i--){
+                    inter=leitor1[i];
+                    leitor1[i]=leitor1[h];
+                    leitor1[h]=inter;
                 }
             }
-            for (h=0;h<=(tam1+tam2)-1;h++){
+            if(leitor2[1]>leitor2[2]){
+                for(i=tam1-1;i>=0;i--){
+                    inter=leitor2[i];
+                    leitor2[i]=leitor2[h];
+                    leitor2[h]=inter;
+                }
+            }
+            tamf=tam1+tam2-1;
+            i=0;
+            j=0;
+            for (h=0;h<=tamf;h++){
+                if(leitor1[i]<leitor2[j]){
+                    final[h]=leitor1[i];
+                    i++;
+                }
+                else{
+                    final[h]=leitor2[j];
+                    j++;
+                }
+            }
+            for(h=0;h<=tamf;h++){
                 printf("%d ",final[h]);
-
             }
         }
-
     }while (tam1!=51);
 }
