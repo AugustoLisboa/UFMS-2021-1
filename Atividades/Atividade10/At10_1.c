02b8d9bdf33a06a652992147b90b2e5397aa67f0
AugustoLisboa <lisbo.augusto98@gmail.com>
2021-06-04 14:05:17 -0400

    atividade 10_1 10/10

--- Atividades/Atividade10/At10_1.c
+++ Atividades/Atividade10/At10_1.c
@@ -6,8 +6,10 @@ int main (void){
     char select[8];
     scanf(" %d",&m);
     scanf(" %d",&n);
-    for (cont=0;cont<=m-1;cont++){
-        for (contx=0;contx<=n-1;contx++){
+    n=n-1;
+    m=m-1;
+    for (cont=0;cont<=m;cont++){
+        for (contx=0;contx<=n;contx++){
                 scanf(" %d",&vetorMN[cont][contx]);
         }
     }
@@ -15,7 +17,7 @@ int main (void){
         scanf(" %s",select);
         if(strcmp(select,"inv_l")==0){
             scanf(" %d",&operator);
-            for (cont=0;cont<=n-1;cont++){
+            for (cont=0;cont<=n/2;cont++){
                 middle=vetorMN[operator][cont];
                 vetorMN[operator][cont]=vetorMN[operator][n-cont];
                 vetorMN[operator][n-cont]=middle;
@@ -23,7 +25,7 @@ int main (void){
         }
         else if(strcmp(select,"inv_c")==0){
             scanf(" %d",&operator);
-            for (cont=0;cont<=m;cont++){
+            for (cont=0;cont<=m/2;cont++){
                 middle=vetorMN[cont][operator];
                 vetorMN[cont][operator]=vetorMN[m-cont][operator];
                 vetorMN[m-cont][operator]=middle;
@@ -31,8 +33,8 @@ int main (void){
 
         }
         else if (strcmp(select,"imprime")==0){
-            for (cont=0;cont<=m-1;cont++){
-                for (contx=0;contx<=n-1;contx++){
+            for (cont=0;cont<=m;cont++){
+                for (contx=0;contx<=n;contx++){
                     printf("%d ",vetorMN[cont][contx]);
                 }
                 printf("\n");
@@ -40,10 +42,10 @@ int main (void){
         }
         else if (strcmp(select,"invl_se")==0){
             scanf(" %d",&operator);
-            for (cont=0;cont<=m-1;cont++){
-                for (contx=0;contx<=n-1;contx++){
+            for (cont=0;cont<=m;cont++){
+                for (contx=0;contx<=n;contx++){
                     if(operator==vetorMN[cont][contx]){
-                        for (contf=0;contf<=n;contf++){
+                        for (contf=0;contf<=n/2;contf++){
                             middle=vetorMN[cont][contf];
                             vetorMN[cont][contf]=vetorMN[cont][n-contf];
                             vetorMN[cont][n-contf]=middle;
@@ -55,10 +57,10 @@ int main (void){
         }
         else if(strcmp(select,"invc_se")==0){
             scanf(" %d",&operator);
-            for (cont=0;cont<=m-1;cont++){
-                for (contx=0;contx<=n-1;contx++){
+            for (contx=0;contx<=n;contx++){
+                for (cont=0;cont<=m;cont++){
                     if(operator==vetorMN[cont][contx]){
-                        for (contf=0;contf<=m;contf++){
+                        for (contf=0;contf<=m/2;contf++){
                             middle=vetorMN[contf][contx];
                             vetorMN[contf][contx]=vetorMN[m-contf][contx];
                             vetorMN[m-contf][contx]=middle;
